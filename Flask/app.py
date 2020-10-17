e245ae87ef8ff22289d515031616f4939e18cf85
Shreyans Jain <shreyansjain68@gmail.com>
2020-10-17 19:12:38 +0530

    fixed basic repo and added requirements.txt

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
     return render_template('form.html')
+
+
+@app.route('/thanks', methods=["GET"])
+def get_thanks():
+    return render_template('ok.html')
 
 @app.route('/thanks', methods=["POST"])
 def thanks():
@@ -13,6 +18,6 @@ def thanks():
     message = request.form['message']
     print(name + ' | ' + email)
     print(message)
-    return render_template('ok.html')
+    return redirect(url_for('get_thanks'))
 
-app.run()
+app.run(debug=True)
